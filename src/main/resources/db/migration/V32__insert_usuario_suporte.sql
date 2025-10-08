eda50c1049572bff1c6536edad43570404e933e5
Vitor Hugo Moreira Souza <vitormoreira@pactosolucoes.com.br>
2025-10-07 23:46:26 -0300

    Ajuste para migration usuário

--- src/main/resources/db/migration/V32__insert_usuario_suporte.sql
+++ src/main/resources/db/migration/V32__insert_usuario_suporte.sql
@@ -1,19 +1,18 @@
 -- V32__insert_usuario_suporte.sql
--- Migration para inserir usuário de suporte
+-- Migration para inserir usuï¿½rio de suporte
 
-INSERT INTO usuario (nome_usuario, email, senha, categoria_id, status, telefone, criado_em)
+INSERT INTO usuario (nome_usuario, email, senha, categoria_id, status, telefone)
 VALUES (
   'suporte',
   'lite-erp-enterprise@gmail.com',
   '$2a$10$7rha9ohG4X4kwIrErL8Mr.dUhVYE94gKhfAPF8.GlW5bbOWctcRkG',
   2,
   'autorizado',
-  '62 99476-8955',
-  now()
+  '62 99476-8955'
 )
 ON CONFLICT (email) DO NOTHING;
 
--- Atualiza sequência de id (ajuste o nome da sequência se for diferente)
+-- Atualiza sequÃªncia de id (ajuste o nome da sequÃªncia se for diferente)
 SELECT setval('usuario_id_seq', GREATEST((SELECT MAX(id) FROM usuario), 1));
 
-COMMENT ON TABLE usuario IS 'Tabela de usuários do sistema';
+COMMENT ON TABLE usuario IS 'Tabela de usuï¿½rios do sistema';
