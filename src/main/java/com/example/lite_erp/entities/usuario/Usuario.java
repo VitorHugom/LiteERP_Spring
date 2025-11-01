fe39b105d8bcbcc100d75798d465d7d91483c9c5
Vitor Hugo Moreira Souza <vitormoreira@pactosolucoes.com.br>
2025-11-01 00:09:13 -0300

    hotfix(Usuario): Correção para ignorar campos não nescessários.

--- src/main/java/com/example/lite_erp/entities/usuario/Usuario.java
+++ src/main/java/com/example/lite_erp/entities/usuario/Usuario.java
@@ -1,6 +1,7 @@
 package com.example.lite_erp.entities.usuario;
 
 import com.example.lite_erp.entities.categoria_usuario.CategoriasUsuario;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
